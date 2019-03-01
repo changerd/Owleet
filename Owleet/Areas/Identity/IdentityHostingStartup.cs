023e70d2572cbf9bcdb2025bc875dfbb19a32d32
LENOVO\Anton <antondedenko@gmail.com>
2019-03-02 00:07:57 +0300

    Добавлено Asp.Net Identity pages.

---
+++ Owleet/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Owleet.Models;
+
+[assembly: HostingStartup(typeof(Owleet.Areas.Identity.IdentityHostingStartup))]
+namespace Owleet.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
