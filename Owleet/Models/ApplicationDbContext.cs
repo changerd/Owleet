12bb8ea70751199997f8090df76f53a61222d527
Slava Morza <bloodreek@gmail.com>
2019-03-01 23:58:44 +0300

    )))

--- Owleet/Models/ApplicationDbContext.cs
+++ Owleet/Models/ApplicationDbContext.cs
@@ -4,13 +4,16 @@ using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace Owleet.Data
+namespace Owleet.Models
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+            Database.EnsureCreated();
         }
+        public DbSet<ApplicationUser> Users { get; set; }
+
     }
 }
